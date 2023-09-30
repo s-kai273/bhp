f41779872873f53082cda630bfe072f7cb566e07
s-kai273 <kinoshita116@gmail.com>
2023-09-30 23:37:52 +0900

    changed rsa key file name

--- network_communication_programs/ssh_server.py
+++ network_communication_programs/ssh_server.py
@@ -6,8 +6,7 @@ import threading
 
 
 CWD = os.path.dirname(os.path.realpath(__file__))
-HOSTKEY = paramiko.RSAKey(filename='/home/kali/.ssh/id_rsa')
-# HOSTKEY = paramiko.RSAKey(filename=os.path.join(CWD,  'test_rsa.key'))
+HOSTKEY = paramiko.RSAKey(filename=os.path.join(CWD,  'test_rsa.key'))
 
 
 class Server(paramiko.ServerInterface):
